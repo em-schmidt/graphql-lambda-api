66788f6d7759989883b59344a00bb3c8a63faad6
eric m. schmidt <eric@atmospheric.cloud>
2017-08-24 12:23:35 -0400

    add dynamodb connector

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 /* 'use strict'; */
 
-import * as server from "apollo-server-lambda";
-import { Schema } from "./data/schema";
+import * as server from 'apollo-server-lambda';
+import { Schema } from './data/schema';
+import { docCLient } from './data/connectors/docClient';
 
-exports.graphqlHandler  =  server.graphqlLambda((event, context) => {
+exports.graphqlHandler = server.graphqlLambda((event, context) => {
   const headers = event.headers;
   const functionName = context.functionName;
 
@@ -13,13 +14,13 @@ exports.graphqlHandler  =  server.graphqlLambda((event, context) => {
       headers,
       functionName,
       event,
-      context
-    }
-  }
+      context,
+      docCLient,
+    },
+  };
 });
 
 exports.graphiqlHandler = server.graphiqlLambda({
-  endpointURL: '/graphql'
-})
-
+  endpointURL: './graphql',
+});
 
